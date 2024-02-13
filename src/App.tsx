34b1de0bbc664ae6fcbad558f043f50148f46c62
Allan Hanauer <allan.hanauer.dev@gmail.com>
2024-02-13 20:03:47 -0300

    Initial Config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
+import Header from "./components/Header";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+//TODO: COMPONENTS
+import ErrorPage from "./pages/ErrorPage.tsx";
 
+//TODO:Variables to main element
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
