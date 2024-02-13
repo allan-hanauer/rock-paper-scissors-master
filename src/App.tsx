cf47fbf0621c4e7bc20db63d266857b8ed9e38e5
Allan Hanauer <allan.hanauer.dev@gmail.com>
2024-02-13 20:05:49 -0300

    Initial Config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
       <Header />
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 };
